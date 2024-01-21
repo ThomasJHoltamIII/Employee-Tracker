b1a8a7ebc3e1d91344acb14771dadcbab1f2fe52
Thomas Holtam <thomasjholtamIII@gmail.com>
2024-01-20 17:39:53 -0700

    Fixed databases to be sourced properly

--- db/schema.sql
+++ db/schema.sql
@@ -9,30 +9,30 @@ CREATE TABLE department (
     department_name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
+);
+
 CREATE TABLE managers (
     manager_id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
+    salary INT NOT NULL,
     FOREIGN KEY (role_id) REFERENCES role(role_id)
 );
 
-CREATE TABLE role (
-    role_id INT NOT NULL PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    department_id INT NOT NULL,
-    salary INT NOT NULL PRIMARY KEY,
-    FOREIGN KEY (department_id) REFERENCES department(department_id)
-);
-
 CREATE TABLE employees (
-    employee_id INT NOT NULL  PRIMARY KEY,
+    employee_id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
-    FOREIGN KEY (salary) REFERENCES role(salary),
-    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    salary INT NOT NULL,
     FOREIGN KEY (manager_id) REFERENCES managers(manager_id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,27 @@
-INSERT INTO department (department_name) VALUES
-  ('Producers'),
-  ('Artists'),
-  ('Writers');
+INSERT INTO department (department_id, department_name) VALUES
+  (1, 'Producers'),
+  (2, 'Artists'),
+  (3, 'Writers');
 
-INSERT INTO managers (manager_id, first_name, last_name, role_id) VALUES
-  (1, 'Stan', 'Lee', 1),
-  (2, 'Jack', 'Kirby', 4),
-  (3, 'Alan', 'Moore', 7);
+INSERT INTO role (role_id, title, department_id) VALUES
+  (1, 'Producer', 1),
+  (2, 'Lead Artist', 2),
+  (3, 'Penciller', 2),
+  (4, 'Colorist', 2),
+  (5, 'Lead Author', 3),
+  (6, 'Author', 3),
+  (7, 'Storyboard Artist', 3);
 
-INSERT INTO role (role_id, title, department_id, salary) VALUES
-  (1, 'Producer', 1, 175000),
-  (2, 'Lead Artist', 2, 150000),
-  (3, 'Penciller', 2, 120000),
-  (4, 'Colorist', 2, 100000),
-  (5, 'Lead Author', 3, 90000),
-  (6, 'Author', 3, 80000),
-  (7, 'Storyboard Artist', 3, 75000);
+INSERT INTO managers (manager_id, first_name, last_name, role_id, salary) VALUES
+  (1, 'Stan', 'Lee', 1, 250000),
+  (2, 'Jack', 'Kirby', 2, 245000),
+  (3, 'Alan', 'Moore', 5, 200000);
 
-INSERT INTO employees (employee_id, first_name, last_name, role_id, manager_id) VALUES
-  (1, 'Chris', 'Claremont', 1, 1),
-  (2, 'Todd', 'McFarlane', 2, 1),
-  (3, 'Jim', 'Lee', 4, 2),
-  (4, 'Frank', 'Miller', 3, 2),
-  (5, 'Neil', 'Gaiman', 5, 3),
-  (6, 'Brian', 'K. Vaughan', 6, 3),
-  (7, 'Amanda', 'Conner', 7, 3);
+INSERT INTO employees (employee_id, first_name, last_name, role_name, role_id, manager_id, salary) VALUES
+  (1, 'Chris', 'Claremont', 'Producer', 1, 1, 120000),
+  (2, 'Todd', 'McFarlane', 'Lead Artist', 2, 1, 115000),
+  (3, 'Jim', 'Lee', 'Colorist', 4, 2, 110000), 
+  (4, 'Frank', 'Miller', 'Penciller', 3, 2, 95000),
+  (5, 'Neil', 'Gaiman', 'Author', 6, 3, 90000),
+  (6, 'Brian', 'K. Vaughan', 'Author', 6, 3, 82000),
+  (7, 'Amanda', 'Conner', 'Storyboard Artist', 7, 3, 80000);
